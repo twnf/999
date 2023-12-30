4faf2e965568db1cccce8473e298d86205b05c53
twnf <62994162+twnf@users.noreply.github.com>
2023-12-30 21:03:23 +0800

    Add files via upload

---
+++ tupian/sd.php
@@ -0,0 +1,35 @@
+[{
+		"name": "推荐",
+		"list": [{
+			"name": "Twnf",
+			"url": " ",
+			"icon": "https://ghproxy.net/https://raw.githubusercontent.com/twnf/999/main/tupian/kefu.jpg",
+			"version": "Twnf"
+		}, {
+			"name": "Twnf",
+			"url": " ",
+			"icon": "https://ghproxy.net/https://raw.githubusercontent.com/twnf/999/main/tupian/kefu.jpg",
+			"version": "Twnf"
+		}]
+	}, {
+		"name": "推荐1",
+		"list": [{
+			"name": "Twnf",
+			"url": "",
+			"icon": "https://ghproxy.net/https://raw.githubusercontent.com/twnf/999/main/tupian/gengxin.png",
+			"version": "软件更新"
+		}, {
+			"name": "Twnf",
+			"url": "",
+			"icon": "https://ghproxy.net/https://raw.githubusercontent.com/twnf/999/main/tupian/kefu.jpg",
+			"version": "联系客服"
+		}, {
+			"name": "Twnf",
+			"url": "",
+			"icon": "https://ghproxy.net/https://raw.githubusercontent.com/twnf/999/main/tupian/zanshang.jpg",
+			"version": "扫码打赏"
+		}]
+	}
+		]
+	}
+]
